531d28efcfd6995fcaa9a9a8d1336af9aebdb3c2
vortexfoto <saso.robic@e-specialisti.si>
2018-02-09 17:04:00 +0100

    Add files via upload

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
 set(CN_PROJECT_NAME "Brazukcoin")
-set(CN_CURRENCY_DISPLAY_NAME "Karbo")
+set(CN_CURRENCY_DISPLAY_NAME "BrazukCoin")
 set(CN_CURRENCY_TICKER "BZK")
-set(CN_VERSION 1.2.1)
+set(CN_VERSION 1.0.0)
